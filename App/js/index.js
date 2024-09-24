488bc5c95ac90af0820f9e967a8e736cb875785e
bruno1994122 <110351313+bruno1994122@users.noreply.github.com>
2024-09-24 12:32:26 -0300

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -21,24 +21,36 @@ xui.Class('App', 'xui.Module',{
             var host=this, children=[], append=function(child){children.push(child.get(0));};
             
             append(
-                xui.create("xui.UI.Button")
-                .setHost(host,"xui_ui_button3")
-                .setLeft("23.61904761904762em")
-                .setTop("9.904761904761905em")
-                .setCaption("Button")
-                .onClick([
+                xui.create("xui.Timer")
+                .setHost(host,"xui_timer1")
+                .onEnd([
                     {
                         "desc" : "Action 1",
-                        "type" : "other",
-                        "target" : "msg",
-                        "args" : [
-                            "oi",
-                            "oi"
-                        ],
-                        "method" : "pop",
-                        "event" : 1
+                        "type" : "control",
+                        "target" : "xui_ui_dialog5",
+                        "args" : [ ],
+                        "method" : "hide"
                     }
                 ])
+            );
+            
+            append(
+                xui.create("xui.UI.Dialog")
+                .setHost(host,"xui_ui_dialog5")
+                .setLeft("18.285714285714285em")
+                .setTop("5.333333333333333em")
+                .setCaption("home")
+            );
+            
+            append(
+                xui.create("xui.UI.Label")
+                .setHost(host,"xui_ui_label1")
+                .setLeft("27.428571428571427em")
+                .setTop("7.619047619047619em")
+                .setWidth("11.885714285714286em")
+                .setHeight("8.076190476190476em")
+                .setCaption("carregando")
+                .setFontSize("3em")
             );
             
             return children;
@@ -49,6 +61,37 @@ xui.Class('App', 'xui.Module',{
         customAppend : function(parent, subId, left, top){
             // "return false" will cause all the internal UI controls will be added to the parent panel
             return false;
+        },
+        events:{
+            "onReady" : [
+                {
+                    "desc" : "Action 1",
+                    "type" : "control",
+                    "target" : "xui_ui_dialog5",
+                    "args" : [ ],
+                    "method" : "show"
+                },
+                {
+                    "desc" : "Action 2",
+                    "type" : "control",
+                    "target" : "xui_timer1",
+                    "args" : [
+                        {
+                            "interval" : 3000
+                        },
+                        {
+                        }
+                    ],
+                    "method" : "setProperties"
+                },
+                {
+                    "desc" : "Action 3",
+                    "type" : "control",
+                    "target" : "xui_timer1",
+                    "args" : [ ],
+                    "method" : "start"
+                }
+            ]
         }
         /*,
         // To determine how properties affects this module
